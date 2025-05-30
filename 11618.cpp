44a3bec5fe96feca9c38c23d22615ca9c5c1b2e0
Spicytang1 <biuoe@naver.com>
2025-05-31 03:36:11 +0900

    branch 1

---
+++ 11618.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <cmath>
+
+typedef long long ll;
+
+
+using namespace std;
+
+int main()
+{
+    cerr<<"hey\n";
+
+    return 0;
+}   
